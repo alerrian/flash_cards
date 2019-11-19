a2801e3410805eb166d77230a4291e4a579764df
Steven Anderson <stevenanderson@SteveMBP.local>
2019-11-19 09:05:00 -0700

    Add turn class and methods

---
+++ lib/turn.rb
@@ -0,0 +1,32 @@
+class Turn
+  attr_accessor :guess, :card
+
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+
+  def guess
+    @guess
+  end
+
+  def card
+    @card
+  end
+
+  def correct?
+    if @guess == @card.answer
+      true
+    else
+      false
+    end
+  end
+
+  def feedback
+    if correct?
+      p "Correct!"
+    else
+      p "Incorrect."
+    end
+  end
+end
