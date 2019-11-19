358472246ad63d3d21d21178465be6ee95fd7078
Steven Anderson <stevenanderson@SteveMBP.local>
2019-11-19 13:08:55 -0700

    Add logic to cards_in_category

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,4 +8,18 @@ class Deck
   def count
     @cards.count
   end
+
+  def cards_in_category(category)
+    # Access cards array
+    # find elements in array that have specific category
+    #   For each element in the array access .category parameter
+    #     compare to card.category
+    #     if same, send to terminal
+    #     else ignore
+    @cards.each |card| do
+      if card.category == category
+        card
+      end
+    end
+  end
 end
