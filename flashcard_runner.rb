0d78aedc2b4f594642ce9a2cb681726064e5c94f
Steven Anderson <steven.a.anderson2012@gmail.com>
2019-11-23 10:16:27 -0700

    Add start method

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,7 +3,22 @@ require './lib/card'
 require './lib/turn'
 require './lib/round'
 
+# Create new cards for the program to run and use
 card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-@cards = [card_1, card_2, card_3]
+
+# Add cards to array for movement to deck
+cards = [card_1, card_2, card_3]
+
+# Add cards to deck
+deck = Deck.new(cards)
+
+# Add deck to new round
+round = Round.new(deck)
+
+# Start program
+
+def start
+  
+end
