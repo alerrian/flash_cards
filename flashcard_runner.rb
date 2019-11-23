61ea7824b1fe54142d2a3e8c3cb062e1edb77aa0
Steven Anderson <stevenanderson@SteveMBP.local>
2019-11-23 14:07:51 -0700

    Create new cards

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -4,12 +4,13 @@ require './lib/turn'
 require './lib/round'
 
 # Create new cards for the program to run and use
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+card_1 = Card.new("What is 5 + 5?", "10", :STEM)
+card_2 = Card.new("What is Rachel's favorite animal?", "Giraffe", :Turing_Staff)
+card_3 = Card.new("What is Mike's middle name?", "Nobody knows", :Turing_Staff)
+card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Pop_Culture)
 
 # Add cards to array for movement to deck
-cards = [card_1, card_2, card_3]
+cards = [card_1, card_2, card_3, card_4]
 
 # Add cards to deck
 deck = Deck.new(cards)
