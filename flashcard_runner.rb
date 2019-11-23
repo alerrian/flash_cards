834dc4d82991a677611c7591921f7f22793655ca
Steven Anderson <stevenanderson@SteveMBP.local>
2019-11-23 15:07:57 -0700

    Finish iteration 3

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -5,12 +5,13 @@ require './lib/round'
 
 # Create new cards for the program to run and use
 card_1 = Card.new("What is 5 + 5?", "10", :STEM)
-card_2 = Card.new("What is Rachel's favorite animal?", "Giraffe", :Turing_Staff)
-card_3 = Card.new("What is Mike's middle name?", "Nobody knows", :Turing_Staff)
-card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Pop_Culture)
+card_2 = Card.new("What is Rachel's favorite animal?", "giraffe", :Turing_Staff)
+card_3 = Card.new("What is Mike's middle name?", "nobody knows", :Turing_Staff)
+card_4 = Card.new("What cardboard cutout lives at Turing?", "justin bieber", :Pop_Culture)
+card_5 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "mars", :STEM)
 
 # Add cards to array for movement to deck
-cards = [card_1, card_2, card_3, card_4]
+cards = [card_1, card_2, card_3, card_4, card_5]
 
 # Add cards to deck
 deck = Deck.new(cards)
--- lib/round.rb
+++ lib/round.rb
@@ -25,14 +25,14 @@ class Round
       @number_correct_by_category[current_card.category] += 1
     end
 
+    p new_turn.feedback
+
     @current_card_number += 1
 
     new_turn
   end
 
   def number_correct_by_category(category)
-    # If guess.correct? then add 1 to the hash value
-    # else just add the category as a key
     @number_correct_by_category[category]
   end
 
@@ -41,7 +41,7 @@ class Round
   end
 
   def percent_correct_by_category(category)
-    return (@number_correct_by_category[category] / @number_per_category[category]) * 100
+    return (@number_correct_by_category[category].to_f / @number_per_category[category]) * 100
   end
 
   def start
@@ -49,8 +49,21 @@ class Round
     p "Welcome! You're playing with #{deck.count} cards."
     p "-------------------------------------------------"
 
-    p "This is card number #{@current_card_number + 1} of #{deck.count}."
-    p "Question: " + deck.cards[@current_card_number].question
-    
+    until @current_card_number >= deck.count do 
+      p "This is card number #{@current_card_number + 1} of #{deck.count}."
+      p "Question: " + deck.cards[@current_card_number].question
+
+      # Get user guess
+      user_guess = gets.chomp
+
+      # Compare user guess to current card answer
+      take_turn(user_guess.downcase)
+    end
+
+    puts "\n****** GAME OVER! ******"
+    p "You had #{@number_correct} correct guesses out of #{deck.count} for a total score of #{percent_correct.round(0)}%."
+    p "STEM - #{percent_correct_by_category(:STEM).round(0)}% correct"
+    p "Turing Staff - #{percent_correct_by_category(:Turing_Staff).round(0)}% correct"
+    p "Pop Culture - #{percent_correct_by_category(:Pop_Culture).round(0)}% correct"
   end
 end
