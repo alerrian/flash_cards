a32fc321e479a6c739ec45e715924b0b725e9cde
Steven Anderson <stevenanderson@SteveMBP.local>
2019-11-23 15:47:15 -0700

    Add class and return functionality to project

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -8,4 +8,10 @@ class CardGeneratorTest < Minitest::Test
 
     assert_instance_of CardGenerator, cards
   end
+
+  def test_filename_is_the_file_name
+    cards = CardGenerator.new()
+
+    asser_equal "cards.txt", cards.filename
+  end
 end
