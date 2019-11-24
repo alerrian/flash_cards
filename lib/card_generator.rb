7c741c6d199cf05c10ea72ff55270d221ef1e83e
Steven Anderson <stevenanderson@SteveMBP.local>
2019-11-24 10:54:25 -0700

    Add test for generator

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,4 +1,6 @@
 class CardGenerator
+  attr_reader :filename
+
   def initialize(filename)
     @filename = filename
   end
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -9,4 +9,10 @@ class CardGeneratorTest < Minitest::Test
 
     assert_instance_of CardGenerator, generated_cards
   end
+
+  def test_filename_is_name_of_file
+    generated_cards = CardGenerator.new("cards.txt")
+
+    assert_equal "cards.txt", generated_cards.filename
+  end
 end
