6d0ebb4d06e253487a4aadd6dff40ecc69bd5865
Steven Anderson <stevenanderson@SteveMBP.local>
2019-11-23 17:39:45 -0700

    Add better guess checking

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -5,10 +5,10 @@ require './lib/round'
 
 # Create new cards for the program to run and use
 card_1 = Card.new("What is 5 + 5?", "10", :STEM)
-card_2 = Card.new("What is Rachel's favorite animal?", "red panda", :Turing_Staff)
-card_3 = Card.new("What is Mike's middle name?", "nobody knows", :Turing_Staff)
-card_4 = Card.new("What cardboard cutout lives at Turing?", "justin bieber", :Pop_Culture)
-card_5 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "mars", :STEM)
+card_2 = Card.new("What is Rachel's favorite animal?", "Red Panda", :Turing_Staff)
+card_3 = Card.new("What is Mike's middle name?", "Nobody knows", :Turing_Staff)
+card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Pop_Culture)
+card_5 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 
 # Add cards to array for movement to deck
 cards = [card_1, card_2, card_3, card_4, card_5]
